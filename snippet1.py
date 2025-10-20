81b02f08446886d3a60d07ae7c67bd4a421e3aaf
Becomeadoctor <sneakyninja389@gmail.com>
2025-10-19 21:29:59 -0400

    snippet

---
+++ snippet1.py
@@ -0,0 +1,12 @@
+#db = open("output.txt", "a")
+#a = "Hello"+1
+#b = "How do you do?"
+#db.write(a+", "+b+"\n")
+
+db = open("output.txt", "a")
+
+a = "Hello " + str(1)
+b = "How do you do?"
+
+db.write(a + ", " + b + "\n")
+db.close()
---
+++ snippet2.py
@@ -0,0 +1,9 @@
+#def findTotal(a):
+#for i in a
+#print(sum(i)*2)
+
+def findTotal(a):
+    total = sum(a) * 2
+    print(total)
+
+findTotal([1, 2, 3, 4])
